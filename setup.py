e834cc351aea9ec205c758dcdf2b9f9f74fd92d1
Satyam Pandey <sp6540788@gmail.com>
2026-07-10 16:06:44 +0530

    my proj structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Satyam Pandey",
+    author_email="sp6540788@gmail.com",
+    packages=find_packages()
+)
